586827521ab9d2422c48668dd5868b30d1b50afc
Danilo <daniloworkdev@gmail.com>
2022-06-03 13:18:42 +0100

    feat(MarcaController): implementado status code nos endpoints

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -20,7 +20,7 @@ class MarcaController extends Controller
         //Metodo estatico
         //$marcas = Marca::all();
         $marcas = $this->marca->all();
-        return  $marcas;
+        return response()->json($marcas, 200);
     }
 
     /**
@@ -45,7 +45,7 @@ class MarcaController extends Controller
         //Metodo estatico
         //$marca = Marca::create($request->all());
         $marca = $this->marca->create($request->all());
-        return $marca;
+        return response()->json($marca, 201);
     }
 
     /**
@@ -60,9 +60,9 @@ class MarcaController extends Controller
         //Sugestão de tipo, type hinting do php
         $marca = $this->marca->find($id);
         if($marca === null){
-            return ['erro' => 'Recurso pesquisado não existe'];
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
         };
-        return $marca;
+        return response()->json($marca, 200);
     }
 
     /**
@@ -94,11 +94,11 @@ class MarcaController extends Controller
        $marca = $this->marca->find($id);
 
        if($marca === null){
-            return ['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'];
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
        };
 
        $marca->update($request->all());
-       return $marca;
+       return response()->json($marca, 200);
     }
 
     /**
@@ -112,10 +112,10 @@ class MarcaController extends Controller
         $marca = $this->marca->find($id);
 
         if($marca === null){
-            return ['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'];
+           return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe'], 404);
         };
 
         $marca->delete();
-        return ['msg' => 'A marca foi removida com sucesso!'];
+          return response()->json(['msg' => 'A marca foi removida com sucesso!'], 201);
     }
 }
