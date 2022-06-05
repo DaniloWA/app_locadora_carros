8426d10217b6510a759a0d002fccf84f95dceab0
Danilo <daniloworkdev@gmail.com>
2022-06-05 18:12:27 +0100

    feat(Marca): repository design pattern - implementando repository

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Marca;
+use App\Repositories\MarcaRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -18,36 +19,29 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-        $marcas = array();
+
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if($request->has('atributos_modelos')){
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:id,'.$atributos_modelos);
+            $atributos_modelos = 'modelos:'.$request->atributos_modelos;
+
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         } else {
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if($request->has('filtro')){
-
-            $filtros = explode(';', $request->filtro);
-            foreach($filtros as $key => $condicao){
-                $c = explode(':', $condicao);
-                $marcas = $marcas->where($c[0],$c[1],$c[2]);
-            };
+            $marcaRepository->filtro($request->filtro);
         }
 
         if($request->has('atributos')){
-            $atributos = explode(',', $request->get('atributos'));
-            $marcas = $marcas->select($atributos)->get();
-        } else {
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($request->atributos);
         };
-
 
         //Metodo estatico
         //$marcas = Marca::all();
         //$marcas = $this->marca->with('modelos')->get();
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
     }
 
     /**
