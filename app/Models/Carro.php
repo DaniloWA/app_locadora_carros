ab22d1343ea5b7528bd9ed133177885ae0cb1b1a
Danilo <daniloworkdev@gmail.com>
2022-06-06 09:55:27 +0100

    feat: modelo Carro criado

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function rules(){
+        return [           //verificando se existe na tabela marcas coluna id
+            'modelo_id' => 'exists:modelos,id',
+            'placa' => 'required',
+            'disponivel' => 'required',
+            'km' => 'required',
+        ];
+    }
+
+    public function feedback() {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+        ];
+    }
+
+    public function modelo(){
+        return $this->belongsTo('App\models\Modelo');
+    }
 }
