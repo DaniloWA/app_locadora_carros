88ce0f9830ee24a42728c6a1960434a1d4e31c88
Danilo <daniloworkdev@gmail.com>
2022-06-06 10:03:26 +0100

    fix: classe Request http

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -5,17 +5,34 @@ namespace App\Http\Controllers;
 use App\Models\Cliente;
 use App\Http\Requests\StoreClienteRequest;
 use App\Http\Requests\UpdateClienteRequest;
+use App\Repositories\ClienteRepository;
+use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
+
+    public function __construct(Cliente $cliente){
+        $this->cliente = $cliente;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $clienteRepository = new ClienteRepository($this->cliente);
+
+        if($request->has('filtro')){
+            $clienteRepository->filtro($request->filtro);
+        }
+
+        if($request->has('atributos')){
+            $clienteRepository->selectAtributos($request->atributos);
+        };
+
+        return response()->json($clienteRepository->getResultado(), 200);
     }
 
     /**
@@ -26,7 +43,17 @@ class ClienteController extends Controller
      */
     public function store(StoreClienteRequest $request)
     {
-        //
+
+        $request->validate($this->carro->rules() ,$this->carro->feedback());
+
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km,
+        ]);
+
+        return response()->json($carro, 201);
     }
 
     /**
