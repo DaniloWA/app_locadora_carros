62bc602ab557fbf7be2a703810b7385c459322ae
Danilo <daniloworkdev@gmail.com>
2022-06-07 10:34:52 +0100

    feat: implementado o método de login(autenticação) e JWT (autorização)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,25 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+
+        $credenciais = $request->all(['email', 'password']);
+
+        //Autentificação do usuario (email e senha)
+        $token =auth('api')->attempt($credenciais);
+
+        if($token){ //user autenticado com sucesso!
+            return response()->json(['token' => $token], 200);
+
+
+        } else {  //erro user ou senha
+            return response()->json(['erro' => 'Usuário ou senha inválido!'], 403);
+
+            // 401 = Unauthorized -> não autorizado. User pode estar logado mas não tem autorização
+            // 403 = forbideen -> proibido (login inválido)
+        }
+
+        //retornar um JWT - Json Web Token
         return 'Login';
     }
 
