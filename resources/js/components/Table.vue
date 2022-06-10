a0a186989c1fac5a99de269ff4eb318670946be1
Danilo <daniloworkdev@gmail.com>
2022-06-10 20:51:57 +0100

    refactor(Table): evitando dependências no componente

--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -3,14 +3,30 @@
         <table class="table table-hover">
         <thead>
             <tr>
-                <th scope="col"  v-for="t, key in titulos" :key="key">{{t}}</th>
+                <th scope="col"  v-for="t, key in titulos" :key="key" class="text-uppercase">{{t}}</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-for="marcas in dados" :key="marcas.id">
+            <tr v-for="obj in dados" :key="obj.id">
+                <template v-for="valor, chave in obj">
+                    <td :key="chave" v-if="titulos.includes(chave)">
+                        <span v-if="chave == 'imagem'">
+                            <img :src="'/storage/' + valor" width="30" height="30" alt="img">
+                        </span>
+                        <span v-else>
+                            {{valor}}
+                        </span>
+                    </td>
+                </template>
+
+
+
+                <!--
                 <th scope="row">{{marcas.id}}</th>
                 <td>{{marcas.nome}}</td>
                 <td><img :src="'/storage/' + marcas.imagem" width="30" height="30" alt="img"></td>
+                -->
+
             </tr>
 
         </tbody>
