ce50b5c96fe8660729f7c2ece4fa345e8e4d4a78
Danilo <daniloworkdev@gmail.com>
2022-06-11 18:11:38 +0100

    feat(marca): logica do modal de visualização

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -32,7 +32,11 @@
                     <template v-slot:conteudo>
                         <table-component
                         :dados="marcas.data"
-                        :visualizar="true"
+                        :visualizar="{
+                            visivel: true,
+                            dataToggle: 'modal',
+                            dataTarget: '#modalMarcaVisualizar'
+                        }"
                         :atualizar="true"
                         :remover="true"
                         :titulos="{
