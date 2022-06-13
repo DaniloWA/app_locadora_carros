394f5cdc89f34d275ffcc4329b57c0d917c0ec03
Danilo <daniloworkdev@gmail.com>
2022-06-13 12:29:00 +0100

    feat: implementado componente index

---
+++ resources/js/components/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+               <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">Gestão de Carros Online</h5>
+                    <p class="card-text">O melhor website para alugar o seu carro preferido!</p>
+                    <p class="text-muted">Conecte-se para ter acesso a aplicação!</p>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
