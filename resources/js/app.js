9078a2a8ad05c9dd2b5b3f104ad82b8023ee6ca3
Danilo <daniloworkdev@gmail.com>
2022-06-17 19:51:59 +0100

    feat: criado view carros e implementado layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -49,6 +49,7 @@ Vue.component('alert-component', require('./components/Alert.vue').default);
 Vue.component('paginate-component', require('./components/Paginate.vue').default);
 Vue.component('index-component', require('./components/Index.vue').default);
 Vue.component('modelos-component', require('./components/Modelos.vue').default);
+Vue.component('carros-component', require('./components/Carros.vue').default);
 
 
 
